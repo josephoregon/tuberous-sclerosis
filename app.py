453c227fe4c841df940497d0e7891abab3932630
Joseph Rosas <josephoregon@icloud.com>
2021-03-13 17:50:26 -0600

    add recommended links and initial format

--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 tuberous-sclerosis
 
 Author: JosephOregon https://github.com/josephoregon
+Website: formaui.org
 Project Notes: For my son, Maui.
 """
 
 import dash
 # import dash_core_components as dcc
+# dash-core-components==1.15.0
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
@@ -15,16 +17,18 @@ app = dash.Dash(__name__, external_stylesheets=stylesheets)
 server = app.server
 
 
-def main():
-    header_title = """ For Maui. """
-    return header_title
+def greetings():
+    return html.H1('For Maui.')
 
 
-greetings = main()
-
-app.layout = html.Div(
-    greetings
-)
+app.layout = html.Div([
+    greetings(),
+    html.H3('Recommended Links'),
+    html.A('Tuberous Sclerosis Warrior 💪🏽', href='https://www.instagram.com/maui__strong/', target='_blank'),
+    html.Br(),
+    html.A('Ketogenic Diet and Hypothalamic Hamartomas', href='https://www.whitneyerd.com/2020/07/vegan-plant-based'
+                                                              '-ketogenic-diet.html', target='_blank')
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
