0a7e131c0d9d30a607fc8fc903c762db2b2bab60
Joseph Rosas <josephoregon@icloud.com>
2021-03-13 17:17:23 -0600

    update requirements.txt to exclude modules not used

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Project Notes: For my son, Maui.
 """
 
 import dash
-import dash_core_components as dcc
+# import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
