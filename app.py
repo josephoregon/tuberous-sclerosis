cfd5592ce3e948b4b2046210896fb488e9f63165
Joseph Rosas <josephoregon@icloud.com>
2021-03-25 02:27:58 -0500

    Add introduction paragraph and a few nav bar changes

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ Author: JosephOregon https://github.com/josephoregon
 Website: formaui.org
 Project Notes: For my son, Maui.
 """
-import os
 import dash
-import dash_html_components as html
 import dash_bootstrap_components as dbc
+import dash_html_components as html
+import dash_core_components as dcc
 
 stylesheets = [dbc.themes.BOOTSTRAP]
 app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
@@ -16,14 +16,9 @@ server = app.server
 favicon = '<link rel="icon" type="image/x-icon" href="{}">'.format('https://for-maui-project.s3.amazonaws.com/images'
                                                                    '/favicon.ico')
 
-app.title = 'For Our Maui.'
+app.title = 'For Maui.'
 
 maui_logo = 'https://for-maui-project.s3.amazonaws.com/images/formaui-logo.png'
-
-
-def greetings():
-    return html.H1('For Maui.')
-
 
 navbar = dbc.Navbar(
     [
@@ -32,30 +27,47 @@ navbar = dbc.Navbar(
             dbc.Row(
                 [
                     dbc.Col(html.Img(src=maui_logo, height="70px")),
-                    dbc.Col(dbc.NavbarBrand("Tuberous Sclerosis Research", className="ml-2")),
+                    dbc.Col(dbc.NavbarBrand("Tuberous Sclerosis Research", className="ml-2"))
                 ],
                 align="center",
                 no_gutters=True,
             ),
             href="https://www.instagram.com/maui__strong/",
         ),
+        dbc.Row(
+                html.H6(
+                    'by Joseph Rosas, Data Scientist', id='nav-bar'
+                )
+        )
     ],
     color="#487e95",
     dark=True,
 )
+
+intro_paragraph = html.Div([
+    html.H3('Introduction'),
+    html.Div([
+        dcc.Markdown('''
+        **SUMMARY:** Self-dedicated research for my son, Maui and all those affected by tuberous-sclerosis (TSC).
+        
+        As my incredible wife continues promoting TSC awareness by using her writing skills, 
+        she inspires me to dedicate a part of my life towards the same purpose.
+        
+        '''),
+        html.Br(),
+        html.Br(),
+        html.H6('Recommended Links'),
+        html.A('Tuberous Sclerosis Warrior 💪🏽', href='https://www.instagram.com/maui__strong/', target='_blank'),
+    ])
+], id='intro-paragraph')
 
 footer = dbc.Container(
     dbc.Row(
         dbc.Col(
             html.P(
                 [
-                    html.Span('by Joseph Rosas, Data Scientist', className='mr-2'),
-                    html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:<you>@<provider>.com'),
-                    html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/<you>/<repo>'),
-                    html.A(html.I(className='fab fa-linkedin mr-1'), href='https://www.linkedin.com/in/<you>/'),
-                    html.A(html.I(className='fab fa-twitter-square mr-1'), href='https://twitter.com/<you>'),
-                ],
-                className='lead'
+                    html.Span('100% Open-Source Research. Code Available On Github, The Rosas Family', id='footer')
+                ]
             )
         )
     )
@@ -64,11 +76,9 @@ footer = dbc.Container(
 app.layout = html.Div([
     navbar,
     html.Br(),
-    # greetings(),
-    html.H6('Recommended Links'),
-    html.A('Tuberous Sclerosis Warrior 💪🏽', href='https://www.instagram.com/maui__strong/', target='_blank'),
+    intro_paragraph,
     footer
-])
+], style={'marginBottom': 100, 'marginTop': 60})
 
 
 def toggle_navbar_collapse(n, is_open):
